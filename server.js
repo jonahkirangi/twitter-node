c6bf49308776f813130245002b6ee1c8921a79fa
Jonah Kirangi <jonahkirangi@gmail.com>
2014-11-16 19:47:42 -0800

    Add routing

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+var express = require('express');
+var app = express();
 var Twit = require('twit');
-var config = require('./config.js');
+var config = require('./config');
+var router = express.Router();
 
 var T = new Twit(config);
 
-// Twit configuration
-
-T.get('users/show', { screen_name: 'jonahkirangi' },  function (err, data, response) {
-  console.log(data);
+// Routing
+router.get('/', function(req, res) {
+  T.get('statuses/user_timeline', { screen_name: 'jonahkirangi', count: '10' },  function (err, data, response) {
+  res.send(data);
+  });
 });
+
+app.use('/', router);
+
+// Start server
+app.listen(3000);
+console.log('Server running on port 3000');
