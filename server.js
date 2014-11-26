5a8cdd6ecc52c196a9fcc56e239ca75b0428ecd0
Jonah Kirangi <jonahkirangi@gmail.com>
2014-11-26 10:39:19 -0800

    Modify server.js and syntax clean up

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ var router = express.Router();
 
 var T = new Twit(config);
 
-// Routing
+// Express Settings
 app.use('/', router);
 app.set('view engine', 'hbs');
 
 router.use(express.static(__dirname + '/app'));
 
 // Get Tweets
-app.get('/getposts', function(req,res){
-  console.log(req.headers.referer.split('=')[1]);
+app.get('/getposts', function(req, res) {
   var searchTerm = req.headers.referer.split('=')[1];
-  T.get('statuses/user_timeline', { screen_name: searchTerm, count: '20'}, function(err, reply) {
-    console.log('Retrieved ' + reply.length + ' tweets from @' + searchTerm);
-    res.send(reply);
+
+  T.get('statuses/user_timeline', { screen_name: searchTerm, count: '20' }, function(err, data) {
+    console.log('Retrieved ' + data.length + ' tweets from @' + searchTerm);
+    res.send(data);
   });
 });
 
